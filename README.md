51b9cfe7e66914430d835d99b61b39199a3d9ee6
Martin Bertolino <martinbertolino@gmail.com>
2022-12-31 11:51:30 -0600

    First commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# HTML and CSS Crash Courses
+
+Resultant files from going through the various Traversy Media Crash Courses
+
+Good cheatsheets
+
+The JavaScript one was very basic.
+
---
+++ jscc/main.js
@@ -0,0 +1,26 @@
+// alert("foobar");
+
+// console.log("foobar")
+// console.error("this is an error")
+// console.warn("this is a warning")
+
+// var, let, const
+// do not use var anymore, scoping
+
+// always use const, unless you can't
+
+// types
+// String, Number, Boolean, null, undefined, Symbol
+
+const name = "John"
+const age = 30
+const rating = 4.5
+const isCool = false
+const x = null
+const y = undefined
+let z // this is undefined also
+
+console.log(typeof name);
+console.log(typeof x);
+console.log(typeof y);
+console.log(typeof z);
